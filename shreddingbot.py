8eefe5089f04b8302f61cca7e7988732a8ca34ee
EgorDeveloper <zavrazhnov.zhora@mail.ru>
2020-08-15 17:45:50 +0300

    mysave

--- shreddingbot.py
+++ shreddingbot.py
@@ -2,9 +2,6 @@ from telethon import TelegramClient, events, sync
 
 import requests
 from bs4 import BeautifulSoup
-import random
-
-import json
 
 # Use your own values from my.telegram.org
 api_id = 1411433
@@ -41,65 +38,5 @@ async def start(event):
         num = 0
 
 
-@bot.on(events.NewMessage(pattern='/мем'))
-async def mem(event):
-    message = event.message.message
-    ls = message.split(' ')
-    print(ls)
-    ls.pop(0)
-    arg = " ".join(ls)
-
-    url = 'https://www.google.co.in/search?q=мем про {}&source=lnms&tbm=isch'.format(arg) 
-
-    #useragent = open('useragents.txt').read().split("\n")
-    #user_agent = {"User-Agent": random.choice(useragent)}
-
-    #proxy = open('proxy.txt').read().split("\n")
-
-    #proxies = {
-     #   'http': random.choice(proxy),
-
-    #}
-
-    r = requests.get(url,)
-    text = r.text
-    soup = BeautifulSoup(text, 'lxml')
-    head = soup.find_all("img",)
-    print(head)
-    res = []
-    b = 0
-    for i in head:
-        res.append(i.get("src"))
-
-    res.pop(0)
-    print(res)
-    res_1 = random.choice(res)
-    chat = event.chat
-    await event.respond(res_1)
-
-'''
-def get_proxy():
-    url = 'http://pubproxy.com/api/proxy?type=https'
-    response = requests.get(url)
-
-    # если адрес забанили или превышен лимит на запросы
-    try:
-        json_proxy = json.loads(response.text)
-    except json.decoder.JSONDecodeError:
-        last_proxy = {'ip_port': read_proxy()}
-        return last_proxy
-
-    # если не забанили, то создаём словарь с данными о прокси
-    proxy_data = dict.fromkeys(['ip_port', 'ip', 'port', 'country', 'last_check'])
-
-    proxy_data['ip_port'] = json_proxy['data'][0]['ipPort']
-    proxy_data['ip'] = json_proxy['data'][0]['ip']
-    proxy_data['port'] = json_proxy['data'][0]['port']
-    proxy_data['country'] = json_proxy['data'][0]['country']
-    proxy_data['last_check'] = json_proxy['data'][0]['last_checked']
-
-    return proxy_data
-
-'''
 with bot:
     bot.run_until_disconnected()
