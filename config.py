4129cf09b43052cfcde9f08ca5dfb775592f60d7
SHREDDING <zavrazhnov.zhora@mail.ru>
2022-04-06 01:00:50 +0300

    release2

---
+++ config.py
@@ -0,0 +1,9 @@
+#1) Login on Web_site https://my.telegram.org/apps
+#2) Register your telegram app
+#3) Add information about API_ID and API_HASH
+#4) Add BOT_TOKEN
+
+
+API_ID = "Your ID"
+API_HASH = "Your Hash"
+BOT_TOKEN = "Bot Token"
